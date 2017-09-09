03d2450d55847463f5bc063965cec2e48ebce2bc
^ <joker314@users.noreply.github.com>
2017-09-10 01:57:28 +0300

    Move to new messages api

--- fetchMessages.js
+++ fetchMessages.js
@@ -18,21 +18,21 @@ function updateMessageCounts() {
 		} else {
 			let alt = Object.keys(users)[index];
 			let request = new XMLHttpRequest();
-			request.open("GET", "https://api.scratch.mit.edu/users/" + alt + "/messages/count");
+			request.open("GET", "https://api.scratch.mit.edu/proxy/users/" + alt + "/activity/count");
 			request.onreadystatechange = function() {
 				if(request.readyState === 4) {
 					if(request.status === 200) {
 						// User was found because a status of 200 was reported.
 						let data = JSON.parse(request.responseText);
-						if(data.count !== users[alt].count)	{
+						if(data.msg_count !== users[alt].count)	{
 							let entry = {};
 							entry[alt] = users[alt];
-							entry[alt].count = data.count;
+							entry[alt].count = data.msg_count;
 							chrome.storage.sync.set(entry);
 							chrome.tabs.query({url: "https://scratch.mit.edu/*"}, function(tabs) {
 								tabs.forEach(function(tab) {
-									console.log(data.count);
-									chrome.tabs.sendMessage(tab.id, {name: alt, count: data.count});
+									console.log(data.msg_count);
+									chrome.tabs.sendMessage(tab.id, {name: alt, count: data.msg_count});
 								});
 							});
 						}
